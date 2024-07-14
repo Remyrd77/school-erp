a91599ad5b2d2fd576ab0defb1f1d678f8225d29
remyrd7 <dassrobin77@gmail.com>
2024-07-14 11:04:21 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Remyrd77/school-erp/precache-manifest.370d8b1608983559d831a1087f91cf0d.js"
+  "/Remyrd77.github.io/school-erp/precache-manifest.161ad1042d1c8dae808e87563278d9e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Remyrd77/school-erp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Remyrd77.github.io/school-erp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
